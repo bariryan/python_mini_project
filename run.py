aa7fcd3c6969a13b751fafb0a1b07cb56995a81d
Barry Ryan <bariryan@gmail.com>
2022-07-25 09:25:29 +0000

    added an input varibe to collect users answer to a yes no question. Also added logic to quit the game if the answer yes is not given

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-print('Welcome to my quiz')
+print('Welcome to the ultimate pub quiz!')
+player_ready = input('do you want to play? ')
+
+print(player_ready)
+
+if player_ready != 'yes':
+    quit()
+
+print('Okay! Lets play :')
