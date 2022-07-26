63e2550279e87d2fa8bef59d20fa06a9cee18e4b
Barry Ryan <bariryan@gmail.com>
2022-07-26 09:10:53 +0000

    added initial question and conditional logic for correct answers

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
 print('Welcome to the ultimate pub quiz!')
 player_ready = input('do you want to play? ')
-
-print(player_ready)
 
 if player_ready != 'yes':
     quit()
 
-print('Okay! Lets play :')
+print('Okay! Lets play :)')
+
+answer = input('What is the answer to everything? ') 
+if answer == '42':
+    print('Correct!')
