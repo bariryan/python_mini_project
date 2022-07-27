fd5dfba3d662771f6d69fcd56926c8a78e0e946f
Barry Ryan <bariryan@gmail.com>
2022-07-27 13:39:27 +0000

    added 5 questions with answers and logic to determine correct answer

--- run.py
+++ run.py
@@ -10,3 +10,29 @@ print('Okay! Lets play :)')
 answer = input('What is the answer to everything? ') 
 if answer == '42':
     print('Correct!')
+else:
+    print('Incorrect!')
+
+answer = input('How many numbers are there on a roulette wheel? ') 
+if answer == '37':
+    print('Correct!')
+else:
+    print('Incorrect!')
+
+    answer = input('What is the value of the letter Q in Scrabble? ') 
+if answer == '10':
+    print('Correct!')
+else:
+    print('Incorrect!')
+
+    answer = input('Nostradamus was famous for making what? ') 
+if answer == 'predictions':
+    print('Correct!')
+else:
+    print('Incorrect!')
+
+    answer = input('What was the middle name of Wolfgang Mozart? ') 
+if answer == 'Amadeuss':
+    print('Correct!')
+else:
+    print('Incorrect!')
