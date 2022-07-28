276335865f9814fa66c95966f860087de216a96a
Barry Ryan <bariryan@gmail.com>
2022-07-28 15:07:06 +0000

    added funtionality to convert all answers given by the user

--- run.py
+++ run.py
@@ -2,13 +2,13 @@
 print('Welcome to the ultimate pub quiz!')
 player_ready = input('do you want to play? ')
 
-if player_ready != 'yes':
+if player_ready.lower() != 'yes':
     quit()
 
 print('Okay! Lets play :)')
 
 answer = input('What is the answer to everything? ') 
-if answer == '42':
+if answer.lower() == '42':
     print('Correct!')
 else:
     print('Incorrect!')
@@ -26,13 +26,43 @@ else:
     print('Incorrect!')
 
     answer = input('Nostradamus was famous for making what? ') 
-if answer == 'predictions':
+if answer.lower() == 'predictions':
     print('Correct!')
 else:
     print('Incorrect!')
 
     answer = input('What was the middle name of Wolfgang Mozart? ') 
-if answer == 'Amadeuss':
+if answer.lower() == 'amadeuss':
+    print('Correct!')
+else:
+    print('Incorrect!')
+
+    answer = input('What was the most downloaded app of 2020? ') 
+if answer.lower() == 'tiktok':
+    print('Correct!')
+else:
+    print('Incorrect!')
+
+    answer = input('Which Italian city is Shakespeare’s Romeo and Juliet set? ') 
+if answer.lower() == 'verona':
+    print('Correct!')
+else:
+    print('Incorrect!')
+
+    answer = input('In the Star Wars series of films what is the name of Han Solo’s Wookie co-pilot? ') 
+if answer.lower() == 'chewbacca':
+    print('Correct!')
+else:
+    print('Incorrect!')
+
+    answer = input('Which pastry is used to make Baklava? ') 
+if answer.lower() == 'filo':
+    print('Correct!')
+else:
+    print('Incorrect!')
+
+    answer = input('The Road to Wigan……what, is a book by George Orwell? ') 
+if answer.lower() == 'pier':
     print('Correct!')
 else:
     print('Incorrect!')
