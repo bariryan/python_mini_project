3aefb54a8a39c89b4ce94f67ae9da683076f9ade
Barry Ryan <bariryan@gmail.com>
2022-08-08 09:39:27 +0000

    added more questions and answers for a total of ten

--- run.py
+++ run.py
@@ -15,77 +15,78 @@ if answer.lower() == '42':
 else:
     print('Incorrect!')
 
+
 answer = input('How many numbers are there on a roulette wheel? ') 
 if answer.lower() == '37':
     print('Correct!')
     score += 1
-
 else:
     print('Incorrect!')
 
-    answer = input('What is the value of the letter Q in Scrabble? ') 
+
+answer = input('What is the value of the letter Q in Scrabble? ') 
 if answer.lower() == '10':
     print('Correct!')
     score += 1
-
 else:
     print('Incorrect!')
 
-    answer = input('Nostradamus was famous for making what? ') 
+
+answer = input('Nostradamus was famous for making what? ') 
 if answer.lower() == 'predictions':
     print('Correct!')
     score += 1
-
 else:
     print('Incorrect!')
 
-    answer = input('What was the middle name of Wolfgang Mozart? ') 
+
+answer = input('What was the middle name of Wolfgang Mozart? ') 
 if answer.lower() == 'amadeuss':
     print('Correct!')
     score += 1
-
 else:
     print('Incorrect!')
 
-    answer = input('What was the most downloaded app of 2020? ') 
+
+answer = input('What was the most downloaded app of 2020? ') 
 if answer.lower() == 'tiktok':
     print('Correct!')
     score += 1
-
 else:
     print('Incorrect!')
 
-    answer = input('Which Italian city is Shakespeare’s Romeo and Juliet set? ') 
+
+answer = input('Which Italian city is Shakespeare’s Romeo and Juliet set? ')
 if answer.lower() == 'verona':
     print('Correct!')
     score += 1
-
 else:
     print('Incorrect!')
-
-    answer = input('In the Star Wars series of films what is the name of Han Solo’s Wookie co-pilot? ') 
+ 
+ 
+answer = input('In the Star Wars series of films what is the name of Han Solo’s Wookie co-pilot? ') 
 if answer.lower() == 'chewbacca':
     print('Correct!')
     score += 1
-
 else:
     print('Incorrect!')
 
-    answer = input('Which pastry is used to make Baklava? ') 
+
+answer = input('Which pastry is used to make Baklava? ') 
 if answer.lower() == 'filo':
     print('Correct!')
     score += 1
-
 else:
     print('Incorrect!')
 
-    answer = input('The Road to Wigan……what, is a book by George Orwell? ') 
+
+answer = input('The Road to Wigan……what, is a book by George Orwell? ') 
 if answer.lower() == 'pier':
     print('Correct!')
     score += 1
-
 else:
     print('Incorrect!')
+
 
 print('You correctly answered ' +  str(score)  + ' questions ')
 print('You incorrectly answered ' + str((score/10) * 100) + '%.')
